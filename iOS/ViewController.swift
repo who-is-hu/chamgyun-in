905a7c86d3817bde442be176dd8d61e5887402e9
Jung Ji Seung <rlsmd9@naver.com>
2021-06-28 22:13:48 +0900

    iOS/chore: 프로젝트 초기 커밋

---
+++ iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by 정지승 on 2021/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
