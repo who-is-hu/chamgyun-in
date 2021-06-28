ebd03251bb9a471a7acb4a8a1b5f5d9b5d4549d9
whoishu <dydgndlaek97@gmail.com>
2021-06-29 01:45:05 +0900

    BE/chore : 프로젝트 초기 커밋
    프로젝트 생성

---
+++ server/src/test/java/com/jh/chamgyunin/ChamgyunInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.chamgyunin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamgyunInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
