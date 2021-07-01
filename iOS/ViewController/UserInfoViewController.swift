ef677d5b6449ab682ade9a2673aed0e25fdf9cdf
Jung Ji Seung <rlsmd9@naver.com>
2021-07-01 21:53:44 +0900

    iOS/feat : kakao 로그인 유저 데이터를 개인정보 UI와 연결

--- iOS/ViewController/UserInfoViewController.swift
+++ iOS/ViewController/UserInfoViewController.swift
@@ -13,18 +13,32 @@ import KakaoSDKCommon
 
 class UserInfoViewController: UIViewController, TagListViewDelegate {
 
-    @IBOutlet weak var containerView: UIView!
+    // MARK: - IBOutlet
     @IBOutlet weak var pointView: UIView!
     @IBOutlet weak var tagListView: TagListView!
+    @IBOutlet weak var userEmailView: UILabel!
+    @IBOutlet weak var userNameView: UILabel!
+    @IBOutlet weak var userProfileImageView: UIImageView!
     
+    // MARK: - override Method
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         pointView.layer.cornerRadius = 5.0
+        userProfileImageView.layer.cornerRadius = 9.0
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // setup user email, name and profile image
+        setUpUserInfo()
         
         // setup tags
         setUpPrepareTags()
+        
     }
 
     // MARK: - IBAction
@@ -60,11 +74,32 @@ class UserInfoViewController: UIViewController, TagListViewDelegate {
 //        tagListView.textFont.bol
         tagListView.delegate = self
         tagListView.textFont = UIFont.boldSystemFont(ofSize: 13)
+        tagListView.removeAllTags()
         
         for tag in tags {
             tagListView.addTag("#\(tag)")
         }
         tagListView.addTag("수정하기")
+    }
+    
+    func setUpUserInfo() {
+        UserApi.shared.me() { user, error in
+            if let error = error {
+                print(error)
+            } else {
+                self.userEmailView.text = user?.kakaoAccount?.email ?? "사용자 동의 필요"
+                self.userNameView.text = user?.kakaoAccount?.profile?.nickname ?? "비공개"
+                
+                if let url: URL = user?.kakaoAccount?.profile?.profileImageUrl {
+                    DispatchQueue.global().async() {
+                        let data = try? Data(contentsOf: url)
+                        DispatchQueue.main.async {
+                            self.userProfileImageView.image = UIImage(data: data!)
+                        }
+                    }
+                }
+            }
+        }
     }
     
     // MARK: - TagListViewDelegate Method
