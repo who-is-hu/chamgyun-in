3869a4ecbb5934ca545ce251d97446a5cd6796d4
whoishu <dydgndlaek97@gmail.com>
2021-07-02 21:15:25 +0900

    BE/feat: 통합테스트 베이스 클래스

---
+++ server/src/test/java/com/jh/chamgyunin/IntergrationTest.java
@@ -0,0 +1,9 @@
+package com.jh.chamgyunin;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+@AutoConfigureWebMvc
+public class IntergrationTest {
+}
