5e1d22491067a79dc233720891c51c25e2aeaaad
whoishu <dydgndlaek97@gmail.com>
2021-07-02 21:16:42 +0900

    BE/feat: User 도메인 클래스와 dao 작성

--- server/src/main/java/com/jh/chamgyunin/domain/user/model/User.java
+++ server/src/main/java/com/jh/chamgyunin/domain/user/model/User.java
@@ -1,12 +1,41 @@
 package com.jh.chamgyunin.domain.user.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
     private String email;
-    private String name;
-    private String password;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "provider")
+    private String provider;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @Builder
+    public User(String email, String nickname, String provider) {
+        this.email = email;
+        this.nickname = nickname;
+        this.provider = provider;
+    }
 }
