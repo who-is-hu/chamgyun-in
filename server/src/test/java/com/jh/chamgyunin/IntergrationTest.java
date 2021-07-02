e460a26eceb61d15d0b9858f627f40555eff3faa
whoishu <dydgndlaek97@gmail.com>
2021-07-02 22:52:42 +0900

    BE/feat: 회원가입 통합 테스트

--- server/src/test/java/com/jh/chamgyunin/IntergrationTest.java
+++ server/src/test/java/com/jh/chamgyunin/IntergrationTest.java
@@ -1,9 +1,20 @@
 package com.jh.chamgyunin;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-@WebMvcTest
-@AutoConfigureWebMvc
+@SpringBootTest
+@Disabled
+@AutoConfigureMockMvc
+@Transactional
 public class IntergrationTest {
+    @Autowired protected MockMvc mvc;
+    @Autowired protected ObjectMapper objectMapper;
 }
---
+++ server/src/test/java/com/jh/chamgyunin/domain/user/api/UserControllerTest.java
@@ -0,0 +1,96 @@
+package com.jh.chamgyunin.domain.user.api;
+
+import com.jh.chamgyunin.IntergrationTest;
+import com.jh.chamgyunin.domain.user.dao.UserRepository;
+import com.jh.chamgyunin.domain.user.dto.signup.SignUpRequest;
+import com.jh.chamgyunin.domain.user.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+class UserControllerTest extends IntergrationTest {
+
+    @Autowired
+    private UserSetUp userSetUp;
+
+    public ResultActions requestSignUp(SignUpRequest dto) throws Exception {
+        return mvc.perform(post("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+                .andDo(print());
+    }
+
+    @Test
+    public void 회원가입_성공() throws Exception {
+        //given
+        final User user = User.builder()
+                .email("test@test.com")
+                .provider("kakao")
+                .nickname("testnick")
+                .build();
+
+        System.out.println(user);
+
+        final SignUpRequest dto = SignUpRequest.builder()
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .provider(user.getProvider())
+                .password("testpw1234")
+                .build();
+
+        //when
+        final ResultActions resultActions = requestSignUp(dto);
+
+        //then
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.email").value(user.getEmail()))
+                .andExpect(jsonPath("$.nickname").value(user.getNickname()))
+                .andExpect(jsonPath("$.provider").value(user.getProvider()))
+        ;
+    }
+
+    @Test
+    public void 회원가입_중복유저_실패() throws Exception {
+        //given
+        User user = User.builder()
+                .email("dup@test.com")
+                .nickname("test")
+                .provider("test")
+                .build();
+        User existUser = userSetUp.saveUser(user);
+
+        final SignUpRequest dto = SignUpRequest.builder()
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .provider(user.getProvider())
+                .password("testpassword")
+                .build();
+
+        //when
+        final ResultActions resultActions = requestSignUp(dto);
+
+        //then
+        resultActions
+                .andExpect(status().is4xxClientError())
+        ;
+    }
+}
+
+@Component
+class UserSetUp {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+}
