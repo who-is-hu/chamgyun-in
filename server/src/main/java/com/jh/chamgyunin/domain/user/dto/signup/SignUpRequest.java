2e756a8101fe063fa87abdaf651ba479437535fc
whoishu <dydgndlaek97@gmail.com>
2021-07-02 21:15:56 +0900

    BE/feat: 회원가입 dto 작성
    가입요청 dto
    응답 dto

---
+++ server/src/main/java/com/jh/chamgyunin/domain/user/dto/signup/SignUpRequest.java
@@ -0,0 +1,40 @@
+package com.jh.chamgyunin.domain.user.dto.signup;
+
+import com.jh.chamgyunin.domain.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class SignUpRequest {
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    @Size(min = 6, max = 12, message = "6자 이상 12자 이하의 비밀번호로 설정해주세요")
+    private String password;
+
+    @NotEmpty
+    @Size(min=1, max = 10, message = "1자 이상 10자 이하의 닉네임을 설정해주세요")
+    private String nickname;
+
+    @NotEmpty
+    private String provider;
+
+    public User toEntity(){
+        User user = User.builder()
+                .email(email)
+                .nickname(nickname)
+                .provider(provider)
+                .build();
+        return user;
+    }
+}
---
+++ server/src/main/java/com/jh/chamgyunin/domain/user/dto/signup/SignUpResponse.java
@@ -0,0 +1,14 @@
+package com.jh.chamgyunin.domain.user.dto.signup;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class SignUpResponse {
+
+    private String email;
+    private String nickname;
+}
