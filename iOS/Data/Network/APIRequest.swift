7a5abbfa60dc350c9b614b91c1983e8049cb4238
Jung Ji Seung <rlsmd9@naver.com>
2021-07-05 14:18:11 +0900

    iOS/fix : API 통신 모듈 범용으로 사용할 수 있도록 수정

--- iOS/Data/Network/APIRequest.swift
+++ iOS/Data/Network/APIRequest.swift
@@ -5,4 +5,100 @@
 //  Created by 정지승 on 2021/07/04.
 //
 
-import Foundation
+import UIKit
+
+class APIRequest {
+    // with nobody
+    private let baseUrl: String = "http://118.67.131.182:8080"
+    static let authSignInUrl: String = "/auth/social/login"
+    
+    func requestGet<T: Codable>(url: String, voType: T.Type, completionHandler: @escaping (Bool, Any) -> Void) {
+        guard let url = URL(string: "\(baseUrl)\(url)") else {
+            print("Error: cannot create url")
+            return
+        }
+        
+        var request: URLRequest = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print("Error: with \(error.debugDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("Error: Did not receive data")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200..<300) ~= response.statusCode else {
+                print("Error: HTTP request failed")
+                return
+            }
+            
+            guard let output = try? JSONDecoder().decode(T.self, from: data) else {
+                print("Error: JSON Data Parsing error")
+                return
+            }
+            
+            completionHandler(true, output)
+        }.resume()
+    }
+    
+    // with body
+    func requestPost<T: Codable>(url: String, method: String, voType: T.Type, param: [String: Any], completionHandler: @escaping (Bool, Any) -> Void) {
+        let sendData = try! JSONSerialization.data(withJSONObject: param, options: [])
+        
+        guard let url = URL(string: "\(baseUrl)\(url)") else {
+            print("Error: cannot create url")
+            return
+        }
+        
+        var request: URLRequest = URLRequest(url: url)
+        request.httpMethod = method
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = sendData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print("Error: with \(error!)")
+                return
+            }
+            
+            guard let data = data else {
+                print("Error: Did not receive data")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                print("Error: HTTP request failed")
+                return
+            }
+            
+            guard (200..<300) ~= response.statusCode else {
+                print("Error: HTTP request failed code is \(response.statusCode)")
+                return
+            }
+            
+            guard let output = try? JSONDecoder().decode(voType, from: data) else {
+                print("Error: JSON Data Parsing error")
+                return
+            }
+            
+            completionHandler(true, output)
+        }.resume()
+    }
+    
+    func request<T: Codable>(url: String, method: String, voType: T.Type, param: [String: Any]? = nil, completionHandler: @escaping (Bool, Any) -> Void) {
+        if method == "GET" {
+            requestGet(url: url, voType: voType) { (success, data) in
+                completionHandler(success, data)
+            }
+        } else {
+            requestPost(url: url, method: method, voType: voType, param: param!) { (success, data) in
+                completionHandler(success, data)
+            }
+        }
+    }
+}
