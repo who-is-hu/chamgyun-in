70f09b3111489eaf176d161c692597bcae56df54
Jung Ji Seung <rlsmd9@naver.com>
2021-07-05 23:42:03 +0900

    iOS/feat : 고민 상세 View Controller 생성 및 Storyboard 바인딩

---
+++ iOS/Scenes/MainView/WorryDetail/WorryDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WorryDetailViewController.swift
+//  iOS
+//
+//  Created by 정지승 on 2021/07/05.
+//
+
+import UIKit
+
+class WorryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
