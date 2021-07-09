348b1129f451431b2eea55199fe6083132c5ae26
whoishu <dydgndlaek97@gmail.com>
2021-07-09 12:00:20 +0900

    BE/feat: session id argument resolver

---
+++ server/src/main/java/com/jh/chamgyunin/global/argumentresolver/GetLoginUserIdArgumentResolver.java
@@ -0,0 +1,29 @@
+package com.jh.chamgyunin.global.argumentresolver;
+
+import com.jh.chamgyunin.domain.auth.service.SessionKey;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@Component
+@RequiredArgsConstructor
+public class GetLoginUserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession session;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginUserId.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return session.getAttribute(SessionKey.LOGIN_USER_ID);
+    }
+}
