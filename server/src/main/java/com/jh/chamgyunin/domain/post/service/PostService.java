8f15c2e6fb355258256434a5b72a4d80dda516b6
whoishu <dydgndlaek97@gmail.com>
2021-07-09 16:11:21 +0900

    BE/feat: @Transactional 추가

--- server/src/main/java/com/jh/chamgyunin/domain/post/service/PostService.java
+++ server/src/main/java/com/jh/chamgyunin/domain/post/service/PostService.java
@@ -1,17 +1,25 @@
 package com.jh.chamgyunin.domain.post.service;
 
+import com.jh.chamgyunin.domain.auth.service.SessionKey;
 import com.jh.chamgyunin.domain.post.dao.PostRepository;
 import com.jh.chamgyunin.domain.post.dto.PostCreateRequest;
 import com.jh.chamgyunin.domain.post.model.Post;
 import com.jh.chamgyunin.domain.user.model.User;
+import com.jh.chamgyunin.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpSession;
+
+@Transactional
 @RequiredArgsConstructor
 public class PostService {
 
-    final PostRepository postRepository;
+    private final PostRepository postRepository;
+    private final UserService userService;
 
-    public Post create(final PostCreateRequest dto, User user){
+    public Post create(final Long userId, final PostCreateRequest dto){
+        User user = userService.findById(userId);
         Post post = dto.toEntity(user);
         return postRepository.save(post);
     }
