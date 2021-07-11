340d935298da3981ec32cb9a20c0c9d76a45ae78
whoishu <dydgndlaek97@gmail.com>
2021-07-11 16:07:41 +0900

    BE/feat: test 환경 분리
    h2 db 사용

--- server/src/test/java/com/jh/chamgyunin/IntegrationTest.java
+++ server/src/test/java/com/jh/chamgyunin/IntegrationTest.java
@@ -1,15 +1,19 @@
 package com.jh.chamgyunin;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+@ActiveProfiles("test")
 @Disabled
 @SpringBootTest
 @AutoConfigureMockMvc
