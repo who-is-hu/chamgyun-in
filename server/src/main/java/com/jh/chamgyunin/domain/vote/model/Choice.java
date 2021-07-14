8be38335747426204eba5532b1236dcd828fe518
whoishu <dydgndlaek97@gmail.com>
2021-07-15 00:47:16 +0900

    BE/feat: Vote, Choice 엔티티 클래스
    BE/feat: 투표 선택지 모델

---
+++ server/src/main/java/com/jh/chamgyunin/domain/vote/model/Choice.java
@@ -0,0 +1,28 @@
+package com.jh.chamgyunin.domain.vote.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "choices")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Choice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "choice_id")
+    private Long id;
+
+    @Column(name = "num_voter", nullable = false)
+    private Long numVoter;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
---
+++ server/src/main/java/com/jh/chamgyunin/domain/vote/model/Vote.java
@@ -0,0 +1,21 @@
+package com.jh.chamgyunin.domain.vote.model;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "votes")
+@Getter
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vote_id")
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "choice_id")
+    private List<Choice> choices;
+}
