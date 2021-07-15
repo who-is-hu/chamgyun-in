031bd9829999c0cd1745d524f141a63834b8068c
whoishu <dydgndlaek97@gmail.com>
2021-07-15 22:07:33 +0900

    BE/feat: Choice id로 조회 서비스

--- server/src/main/java/com/jh/chamgyunin/domain/vote/service/ChoiceService.java
+++ server/src/main/java/com/jh/chamgyunin/domain/vote/service/ChoiceService.java
@@ -1,6 +1,7 @@
 package com.jh.chamgyunin.domain.vote.service;
 
 import com.jh.chamgyunin.domain.vote.dao.ChoiceRepository;
+import com.jh.chamgyunin.domain.vote.exception.ChoiceNotFoundException;
 import com.jh.chamgyunin.domain.vote.model.Choice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,11 @@ public class ChoiceService {
     public Choice insertChoice(final String name) {
         return choiceRepository.save(Choice.of(name));
     }
+
+    public Choice findById(final Long id) {
+        return choiceRepository.findById(id).orElseThrow(()->new ChoiceNotFoundException(id));
+    }
+    
 
 
 }
